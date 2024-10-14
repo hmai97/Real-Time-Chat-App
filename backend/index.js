80cb71ee3276bc458472828885c86e534f7eb808
HUONG MAI <130521221+hmai97@users.noreply.github.com>
2024-10-14 14:20:05 -0700

    backend functions

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require("express"); //our http server
+const cors = require("cors"); // call this from any other origin
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
-
+//rest.chatengine.io for docs
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+  const { username } = req.body; //take username from req body
+  
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username},
+      {headers :{"private-key" : "8433100e-d9ff-4723-ab7f-62fdd19ba856"}}
+    )
+      return res.status(r.status).json(r.data)
+    } catch(e){
+      return res.status(e.response.status).json(e.response.data)
+    }
+  });
 
-app.listen(3001);
+app.listen(3001); //port 3001
